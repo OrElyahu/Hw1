0a10fd1f9e6d4cb152465e88f7c3013ba861afcf
OrElyahu <student44now@gmail.com>
2022-02-09 23:50:03 +0200

    Added Splash Screen

---
+++ PartyQuick/app/src/main/java/com/example/partyquick/SplashScreen.java
@@ -0,0 +1,50 @@
+package com.example.partyquick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    TextView appName;
+    LottieAnimationView lottie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        findViews();
+        beginAnimation();
+        switchActivity();
+
+    }
+
+    private void switchActivity() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+            }
+        },5000);
+    }
+
+    private void beginAnimation() {
+        appName.animate().translationY(-1400).setDuration(2700).setStartDelay(0);
+        lottie.animate().translationX(2000).setDuration(2000).setStartDelay(2900);
+    }
+
+    private void findViews() {
+        appName = findViewById(R.id.appName);
+        lottie = findViewById(R.id.lottie);
+    }
+
+
+
+}
